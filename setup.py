287d583a693bc527c16fe190d778d52dc9a5fe8d
hlavian <hagar.lavian@gmail.com>
2023-09-15 13:16:14 +0200

    making the repo installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='bouterin_private',
+      version='0.1',
+      description='Hagar analysis of Stytra data',
+      url='http://github.com/hlavian/bouterin_private',
+      author="Hagar Lavian",
+      author_email='lavian@neuro.mpg.de',
+      python_requires=">=3.5",
+      license="GNU General Public License v3",
+      packages=['bouterin_private'],
+      zip_safe=False)
+
